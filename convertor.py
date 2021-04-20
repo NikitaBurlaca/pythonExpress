666b7f48699b9db6ed69340b0f9a1c6e1361175f
NikitaBurlaca <80481575+NikitaBurlaca@users.noreply.github.com>
2021-04-20 19:59:52 +0200

    first commit

---
+++ convertor.py
@@ -0,0 +1,8 @@
+print("Enter temperature in celsius: ")
+celsius_value = input()
+celsius_value = float(celsius_value)
+
+
+print(f"Kelvin value is: {celsius_value + 273}")
+print(f"Fahrenheit value is: {celsius_value + 32}")
+
